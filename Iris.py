9c784e0ac2e9d3ddd8d11be9c7ea0889f68b32e3
Samtameus <tuangsamte@outlook.com>
2026-04-27 18:27:00 +0200

    added normalization of data

--- Iris.py
+++ Iris.py
@@ -5,23 +5,31 @@ import matplotlib.pyplot as plt
 iris = load_iris()
 X = iris.data 
 Y = iris.target
-
 ## print("Features:\n", X[:5].tolist())  
 ## print("Labels:\n", Y[:5].tolist())    
 ## print("Target Names: \n", iris.target_names.tolist()) 
 
 #Splitting the dataset into training and testing sets.
 
-X_train = np.vstack([X[0:30], X[50:80], X[100:130]]) #consist of 30 samples (flower and 4 features )from each class
+X_train_raw = np.vstack([X[0:30], X[50:80], X[100:130]]) #consist of 30 samples (flower and 4 features )from each class
 Y_train = np.hstack([Y[0:30], Y[50:80], Y[100:130]]) #labels (type of flower)
 
-X_test = np.vstack([X[30:50], X[80:100], X[130:150]])
+X_test_raw = np.vstack([X[30:50], X[80:100], X[130:150]])
 Y_test = np.hstack([Y[30:50], Y[80:100], Y[130:150]])
 
 ## print("\nX_train shape:", X_train.shape)  # Should be (90, 4)
 ## print("Y_train shape:", Y_train.shape)  # Should be (90,)
 ## print("X_test shape:", X_test.shape)   # Should be (60, 4)
 ## print("Y_test shape:", Y_test.shape)   # Should be (60,)
+
+# ------ Normalising the dataset ---------
+
+mu = X_train_raw.mean(axis=0)  # mean of each column (feature)
+std = X_train_raw.std(axis=0)    # std of each column (feature)
+
+X_train = (X_train_raw - mu) / std
+X_test  = (X_test_raw  - mu) / std  
+
 
 ## print("\nTraining labels:", Y_train)# should contain 30 samples of each class (0, 1, 2)
 #Helping functions 
@@ -137,11 +145,20 @@ print("Error rate:", error_rate(Y_test, pred_test))
 
 # ---------- last 30 of each ----------------- 
 
-X_train2 = np.vstack([X[30:60], X[70:100], X[120:150]])
+X_train2_raw = np.vstack([X[30:60], X[70:100], X[120:150]])
 Y_train2 = np.hstack([Y[30:60], Y[70:100], Y[120:150]])
 
-X_test2 = np.vstack([X[0:20], X[50:70], X[100:120]])
+X_test2_raw = np.vstack([X[0:20], X[50:70], X[100:120]])
 Y_test2 = np.hstack([Y[0:20], Y[50:70], Y[100:120]])
+
+# ------ Normalising the dataset ---------
+
+mu = X_train2_raw.mean(axis=0)  # mean of each column (feature)
+std = X_train2_raw.std(axis=0)    # std of each column (feature)
+
+X_train2 = (X_train2_raw - mu) / std
+X_test2  = (X_test2_raw  - mu) / std  
+# -----------------------------------
 
 X_train2_b = add_bias(X_train2)
 X_test2_b = add_bias(X_test2)   
@@ -181,7 +198,7 @@ for features in range(4):
                              color=colors[classes],
                              bins=15 ) 
     axes[features].set_title(feature_names[features])
-    axes[features].set_xlabel('Value (cm)')
+    axes[features].set_xlabel('Normalized Value')
     axes[features].set_ylabel('count')
     axes[features].legend()
 
@@ -196,6 +213,7 @@ print('\n')
 
 X_train_3f = np.delete(X_train, 1 , axis=1)  # remove sepal width
 X_test_3f  = np.delete(X_test, 1 , axis=1)  
+
 
 X_train_3f_b = add_bias(X_train_3f)
 X_test_3f_b  = add_bias(X_test_3f)  
@@ -235,6 +253,7 @@ print("Test error rate:", error_rate(Y_test, pred_test_3f))
 X_train_2f = np.delete(X_train, [0, 1] , axis=1)  # remove sepal length and sepal width
 X_test_2f  = np.delete(X_test, [0, 1] , axis=1)
 
+
 X_train_2f_b = add_bias(X_train_2f)
 X_test_2f_b  = add_bias(X_test_2f)
 
@@ -270,6 +289,8 @@ print("Test error rate:", error_rate(Y_test, pred_test_2f))
 X_train_1f = np.delete(X_train, [0, 1, 3] , axis=1)  # remove sepal length, sepal width, and petal length
 X_test_1f  = np.delete(X_test, [0, 1, 3] , axis=1)
 
+
+
 X_train_1f_b = add_bias(X_train_1f)
 X_test_1f_b  = add_bias(X_test_1f)
 
@@ -290,7 +311,7 @@ print("Final MSE (1 feature):   ", round(mse_history_1f[-1], 4))
 pred_train_1f = predict(X_train_1f_b, W_1f)
 pred_test_1f  = predict(X_test_1f_b,  W_1f)
 
-print("\n=== 1 FEATURE: Petal Width ===")
+print("\n=== 1 FEATURE: Petal Length ===")
 
 print("\nTraining confusion matrix:")
 print(confusion_matrix(Y_train, pred_train_1f))
